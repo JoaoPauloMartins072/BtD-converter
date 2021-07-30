57cbc5fa2a8d9c9ad9fe37f15291c8c28beab67d
JoaoPauloMartins072 <joaopaulo_072@outlook.com>
2021-07-30 13:49:33 -0300

    parte 2

--- conversor.js
+++ conversor.js
@@ -1,5 +1,18 @@
-function calc() {
-    num1=num1
-    output(num1)
-    
+function converter() {
+  String; binario = document.getElementById("binario").value;
+  String; decimal = document.getElementById("decimal").value;
+  const value = binario.value;
+
+  if ((binario != 0 && binario != 1)) {
+  //if ((length >= 3)) {
+    alert("numero invalido");
+  } else {
+
+    console.log(binario, decimal);
+  }
 }
+
+
+
+
+a
